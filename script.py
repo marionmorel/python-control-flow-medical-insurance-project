7da0972c82a5a36b5b568f27558d97c502df29c7
marionmorel <marion.mrl@live.fr>
2026-02-01 11:16:20 +0000

    Tasks 1, 2 and 3

--- script.py
+++ script.py
@@ -1,8 +1,10 @@
 # Add your code here
- 
+def analyze_smoker(smoker_status):
+  print(smoker_status)
+
 # Function to estimate insurance cost:
 def estimate_insurance_cost(name, age, sex, num_of_children, smoker):
-  estimated_cost = 400*age - 128*sex + 425*num_of_children + 10000*smoker - 2500
+  estimated_cost = 400 * age - 128 * sex + 425 * num_of_children + 10000 * smoker - 2500
   print(name + "'s Estimated Insurance Cost: " + str(estimated_cost) + " dollars.")
   return estimated_cost
  
